449ec5341f63c5545838d5ef0929e090bba22dbb
charroux <benoit.charroux@free.fr>
2014-03-24 16:02:04 +0100

    sauvegarde dans data store

--- src/com/images/GestionProfilServlet.java
+++ src/com/images/GestionProfilServlet.java
@@ -5,6 +5,10 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+
 @SuppressWarnings("serial")
 public class GestionProfilServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
@@ -18,7 +22,15 @@ public class GestionProfilServlet extends HttpServlet {
 		String nom = req.getParameter("nom");
 		String a = req.getParameter("age");
 		int age = Integer.parseInt(a);
-		System.out.println(nom + " " + age);
+		
+		DatastoreService dataStore = DatastoreServiceFactory.getDatastoreService();
+		Entity utilisateur = new Entity("utilisateurs");
+		utilisateur.setProperty("nom", nom);
+		utilisateur.setProperty("age", age);
+		dataStore.put(utilisateur);
+		
+		resp.sendRedirect("/rechercheProfil");
+		
 	}
 	
 	
